eaa4b9e1cffa1f5f34a3aa608ffdaed3475eab90
zhaleh <zhalehelmi@gmail.com>
2024-04-01 19:10:05 +0330

    add js resume

--- script/script.js
+++ script/script.js
@@ -1,9 +1,19 @@
 const navToggleIcon = document.querySelector(".nav__toggle-icon");
 const menu = document.querySelector(".menu");
 const cover = document.querySelector(".cover");
+const resumeListItems = document.querySelectorAll(".resume-list__item");
 
 navToggleIcon.addEventListener("click", function () {
   this.classList.toggle("nav__toggle-icon--open");
   menu.classList.toggle("menu--open");
   cover.classList.toggle("cover--show");
+});
+
+resumeListItems.forEach((resumeListItem) => {
+  resumeListItem.addEventListener("click", function () {
+    document
+      .querySelector(".resume-list__item--active")
+      .classList.remove("resume-list__item--active");
+    this.classList.add("resume-list__item--active");
+  });
 });
